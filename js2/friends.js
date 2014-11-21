aee6d7d5c9b65fe929be07abb3740fdf16822e5c
Urec2731 <chman@ukr.net>
2014-11-21 19:24:46 +0200

    new
    new

--- js2/friends.js
+++ js2/friends.js
@@ -1,19 +1,17 @@
-document.getElementById('contacts').onclick= myClick;
-
-
-function myClick (event){
+document.getElementById('contacts').addEventListener("click",function (event){
+    event.stopPropagation();
     var target = event.target || event.srcElement;
 
-    if (target.nodeName.toString()=='LI')
-        target.className= (target.className)?'':'prepared to delete';
+    if (target.nodeName.toString()=='LI'){
+        target.className= (target.className)?'':'prepared to delete'};
 
-    if(target.className.toString()=='DelButton')
-        target.parentNode.parentNode.removeChild(target.parentNode);
+    if(target.className.toString()=='DelButton'){
+        target.parentNode.parentNode.removeChild(target.parentNode)};
 
-    if(target.className.toString()=='DelAll Selected')
-       for(i=0;i<target.parentNode.childNodes.length;i++)
-           if(target.parentNode.childNodes.item(i).className == 'prepared to delete')
-               target.parentNode.removeChild(target.parentNode.childNodes.item(i));
+    if(target.className.toString()=='DelAll Selected'){
+       for(i=0;i<target.parentNode.childNodes.length;i++){
+           if(target.parentNode.childNodes.item(i).className == 'prepared to delete'){
+               target.parentNode.removeChild(target.parentNode.childNodes.item(i))}}};
 
 
-}
+});
