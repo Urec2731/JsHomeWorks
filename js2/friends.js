c52e8d9d99a406c7a047301d53adfe439200e3af
Urec2731 <chman@ukr.net>
2014-11-23 10:37:53 +0200

    New Version
    jQuery Edition

--- js2/friends.js
+++ js2/friends.js
@@ -1,17 +1,16 @@
-document.getElementById('contacts').addEventListener("click",function (event){
+$('#contacts').click(function (event){
     event.stopPropagation();
     var target = event.target || event.srcElement;
 
     if (target.nodeName.toString()=='LI'){
-        target.className= (target.className)?'':'prepared to delete'};
+        $(target).toggleClass('prepared to delete');
+    };
 
     if(target.className.toString()=='DelButton'){
-        target.parentNode.parentNode.removeChild(target.parentNode)};
+        $(target.parentNode).remove();
+    };
 
     if(target.className.toString()=='DelAll Selected'){
-       for(i=0;i<target.parentNode.childNodes.length;i++){
-           if(target.parentNode.childNodes.item(i).className == 'prepared to delete'){
-               target.parentNode.removeChild(target.parentNode.childNodes.item(i))}}};
-
-
+        $('#contacts>.prepared.to.delete').remove();
+    };
 });
