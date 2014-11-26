aa39982e426af048902f3c5382e0a95d39b58c87
Sergii Kliuchnyk <redexp@ukr.net>
2014-11-26 20:15:54 +0200

    Update friends.js

--- js2/friends.js
+++ js2/friends.js
@@ -1,18 +1,30 @@
-$('#contacts').click(function (event){
-    event.stopPropagation();
-    var target = event.target,
-        $target = $(target);
+jQuery(function ($) {
+    
+   /* $('#contacts').click(function (event){
+        //event.stopPropagation();
+        var target = event.target,
+            $target = $(target);
+    
+        if ($target.attr('data-friend') !== undefined){
+            $target.toggleClass('selected');
+        }
+    
+        else if ($target.attr('data-delete') !== undefined){
+            $target.closest('[data-friend]').remove();
+        }
+    });*/
+    
+    $('#contacts')
+        .on('click', '[data-friend]', function (e) {
+            $(e.target).toggleClass('selected');
+        })
+        .on('click', '[data-delete]', function (e) {
+            $(e.target).closest('[data-friend]').remove();
+        })
+    ;
+    
+    $('#delete-all').click(function () {
+        $('#contacts .selected').remove();
+    });
 
-    if (target.nodeName === 'LI'){
-        $target.toggleClass('prepared-to-delete');
-    }
-
-    else if ($target.hasClass('DelButton')){
-        $target.parent().remove();
-        
-    }
-
-    else if ($target.hasClass('DelAll')){
-        $('#contacts > .prepared-to-delete').remove();
-    }
 });
