f60664bf953f6bea903d609507bfe72babba8828
Urec2731 <chman@ukr.net>
2014-12-05 23:41:30 +0200

    Updated
    separate code & design

--- js4/friends.js
+++ js4/friends.js
@@ -1,19 +1,22 @@
-jQuery.fn.hasAttr= function (Attr){return this.attr(Attr)!== undefined;}; // Копипаст с прошлой домашки
+jQuery.fn.hasAttr = function (Attr){return this.attr(Attr)!== undefined;};
+jQuery.fn.referenceHtml = function () {return this[0].childNodes[1].data};
 jQuery(function ($) {
     
 
     
     $('#contacts')
         .on('click', '[data-friend]', function (e) {
-            var $target=$(e.target); // тут оригинальный код из папки js2 собранный на уроке срабатывал на потомках лишки
+            var $target=$(e.target);
             if ($target.hasAttr('data-friend')){$target.toggleClass('selected');}
         })
         .on('click', '[data-delete]', function (e) {
             $(e.target).closest('[data-friend]').remove();
         })
         .on('click', '[data-add-new-friend]', function () {
+            var splitedtext = $('#contacts').referenceHtml().split('$$$$$');       // получение референсной лишки
+            /////////////////////////////////////////////////////                  // на стороне html выглядит вроде удобно
             var $name=$('#contacts [data-newname]')[0];
-            if (!!$name.value){$('#contacts > li:last-child').before('<li data-friend><input type="text" class="friends-name" value="'+ $name.value +'" /><button data-delete>Delete</button></li>');
+            if (!!$name.value){$('#contacts > li:last-child').before(splitedtext[0] + $name.value + splitedtext[1]);
                 $name.value='';
             }
         })
